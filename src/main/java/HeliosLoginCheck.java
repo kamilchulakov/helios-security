fba526619fdbc65999ff7643226dd5c914da30f1
ulyanovskk <mr1english4@gmail.com>
2021-05-11 18:42:15 +0300

    Update structure

---
+++ src/main/java/HeliosLoginCheck.java
@@ -0,0 +1,7 @@
+import java.util.regex.Pattern;
+
+public class HeliosLoginCheck {
+    public static boolean checkIfContainsHeliosLogin(String word) {
+        return Pattern.matches("\"s(\\d){6}\"", word);
+    }
+}
--- src/test/java/HeliosTest.java
+++ src/test/java/HeliosTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-public class AppTest {
+public class HeliosTest {
     @Test
     public void test_main() throws CheckFailure {
         FileSearch.main(new String[]{""});
