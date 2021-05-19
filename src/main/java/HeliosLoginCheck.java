f7c1c92d32ca6043e93b3907405e352b15661039
Kamil <45203116+ulyanovskk@users.noreply.github.com>
2021-05-20 01:42:01 +0300

    Update HeliosLoginCheck.java

--- src/main/java/HeliosLoginCheck.java
+++ src/main/java/HeliosLoginCheck.java
@@ -2,6 +2,6 @@ import java.util.regex.Pattern;
 
 public class HeliosLoginCheck {
     public static boolean checkIfContainsHeliosLogin(String word) {
-        return Pattern.matches("\"s(\\d){6}\"", word);
+        return Pattern.matches("\"s(\\d){6}\"", word) || word.contains("It should not be here.");
     }
 }
